dfa7b6fe26e306ba890e63276d16d623c9cce6fc
Zach Williams <hey@zachwill.com>
2012-02-13 14:12:44 -0500

    Add working map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 var map = new L.Map('map', {
-  center: new L.LatLng(51.505, -0.09),
-  zoom: 6,
-  layers: new L.TileLayer('http://a.tiles.mapbox.com/v3/zachwill.tunisia/{z}/{x}/{y}.png')
+  center: new L.LatLng(34.2890, 9.3604),
+  zoom: 7,
+  layers: new L.TileLayer('https://tiles.mapbox.com/v3/zachwill.tunisia/{z}/{x}/{y}.png')
 });
