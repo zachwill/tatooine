8583f2ea979b0515968d7ec8ade67bff24e9a2a0
Zach Williams <hey@zachwill.com>
2012-02-13 14:28:56 -0500

    Add min and max zoom levels

--- js/map.js
+++ js/map.js
@@ -1,5 +1,7 @@
 var map = new L.Map('map', {
   center: new L.LatLng(34.2890, 9.3604),
-  zoom: 7,
-  layers: new L.TileLayer('https://tiles.mapbox.com/v3/zachwill.tunisia/{z}/{x}/{y}.png')
+  layers: new L.TileLayer('https://tiles.mapbox.com/v3/zachwill.tunisia/{z}/{x}/{y}.png'),
+  minZoom: 6,
+  maxZoom: 12,
+  zoom: 7
 });
